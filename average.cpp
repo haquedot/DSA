ebc307aeb47deb0a1f66f0ddf62af934afe4789e
Meraj9570 <haquemeraj95@gmail.com>
2022-12-29 21:14:15 +0530

    d

---
+++ average.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int num1, num2, num3;
+	float avg;
+	cout<<"Enter three values : "<<endl;
+	cin>>num1>>num2>>num3;
+	
+	avg = (num1 + num2 + num3)/3;
+	
+	cout<<"Average : "<<avg;
+	
+}
---
+++ comparision.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1,num2;
+
+    cout<<"Enter first value :"<<endl;
+    cin>>num1;
+
+    cout<<"Enter second value :"<<endl;
+    cin>>num2;
+
+    if(num1>num2){
+        cout<<"num1 is greater than num2."<< endl;
+    }
+}
